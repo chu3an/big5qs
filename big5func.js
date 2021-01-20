1415b9622f9fd9d6898672d4b35f22f711df94bf
chu3an <chuan9129@gmail.com>
2021-01-20 22:01:34 +0800

    Release 1.0

--- big5func.js
+++ big5func.js
@@ -1,6 +1,7 @@
 /** Data source url */
 var dataURL = 'https://chu3an.github.io/big5qs/big5code.json'
 
+/** 更新表格 */
 function requestsJson() {
     var myText = document.getElementById("theText").value;
     var requests = new XMLHttpRequest();
@@ -19,15 +20,22 @@ function requestsJson() {
                 cell1.innerHTML = textArray[i];
                 var cell2 = row.insertCell(1);
                 cell2.innerHTML = jsonObj[textArray[i]];
-                console.log(jsonObj[textArray[i]]);
+                //console.log(jsonObj[textArray[i]]);
             }
         }
     };
-    document.getElementById("theResult").style = "visibility:unset"
 }
 
-/** input onclick event */
-document.getElementById("theBtn").onclick = function () { requestsJson() };
+/** theBtn onclick event */
+document.getElementById("theBtn").onclick = function () { 
+    document.getElementById("theBox").style = "visibility:unset"
+    requestsJson()
+};
+
+/** theClr onclick event */
+document.getElementById("theClr").onclick = function () {
+    document.getElementById("theBox").style = "visibility:hidden"
+    document.getElementById("theResult").innerHTML = "<thead id=\"theHead\" class=\"thead-light\"><tr><th>文字</th><th>內碼</th></tr></thead><tbody id=\"theBody\"><tr></tr></tbody>"};
 
 /** input enter hot key support */
 var input = document.getElementById("theText");
