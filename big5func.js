4dd015b6872e7dd2dad13f16eaa0dac884b945b7
chu3an <chuan9129@gmail.com>
2021-01-24 06:26:36 +0800

    test real-time

--- big5func.js
+++ big5func.js
@@ -1,47 +1,58 @@
-/** Data source url */
+/** 資料源 */
 var dataURL = 'https://chu3an.github.io/big5qs/big5code.json'
 
-/** 更新表格 */
-function requestsJson() {
-    var myText = document.getElementById("theText").value;
-    var requests = new XMLHttpRequest();
-    requests.open("GET", dataURL, true);
-    requests.send();
-    requests.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            // Get jsonObj
-            var jsonObj = JSON.parse(this.responseText);
-            // Loop finding
-            var textArray = myText.split("")
-            for (var i = 0; i < textArray.length; i++) {
-                var table = document.getElementById("theResult");
-                var row = table.insertRow(-1);
-                var cell1 = row.insertCell(0);
-                cell1.innerHTML = textArray[i];
-                var cell2 = row.insertCell(1);
-                cell2.innerHTML = jsonObj[textArray[i]];
-                //console.log(jsonObj[textArray[i]]);
-            }
+const theInput = document.getElementById('theInput');
+const theBox = document.getElementById("theBox");
+const theResult = document.getElementById("theResult");
+var theJson = '';
+
+theInput.addEventListener('input', updateResult);
+getJson(dataURL);
+
+function getJson(url) {
+    fetch(url).then(
+        function (r) { return r.json(); }
+    ).then(
+        function (j) {
+            theJson = j;
         }
-    };
+    )
 }
 
-/** theBtn onclick event */
+function updateResult(e) {
+    theBox.style = "visibility:unset";
+    clearResult();
+    const myText = e.srcElement.value;
+    var textArray = myText.split("")
+    for (var i = 0; i < textArray.length; i++) {
+        var table = document.getElementById("theResult");
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML = textArray[i];
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = theJson[textArray[i]];
+        //console.log(jsonObj[textArray[i]]);
+    }
+}
+
+function clearResult() {
+    theResult.innerHTML = '<thead id="theHead" class="thead-light"><tr><th>文字</th><th>內碼</th></tr></thead><tbody id="theBody"><tr></tr></tbody>';
+}
+
 document.getElementById("theBtn").onclick = function () {
-    document.getElementById("theBox").style = "visibility:unset"
-    requestsJson()
+    /** No function temporarily */
 };
 
-/** theClr onclick event */
 document.getElementById("theClr").onclick = function () {
     document.getElementById("theBox").style = "visibility:hidden"
-    document.getElementById("theResult").innerHTML = "<thead id=\"theHead\" class=\"thead-light\"><tr><th>文字</th><th>內碼</th></tr></thead><tbody id=\"theBody\"><tr></tr></tbody>"};
+    clearResult();
+};
 
-/** input enter hot key support */
-var input = document.getElementById("theText");
-input.addEventListener("keyup", function (event) {
+/**
+theInput.addEventListener('keyup', function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
         document.getElementById("theBtn").click();
     }
 });
+*/
